24bdbc9904239136703721b5a28f255b90c85d80
june <git@jml.sh>
2025-09-17 15:04:21 -0700

    still having fun

---
+++ beef.glsl
@@ -0,0 +1,45 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+vec2 N22(vec2 p) {
+  vec3 a = fract(p.xyx * vec3(123.34, 234.34, 345.65));
+  a += dot(a, a + 34.45);
+  return fract(vec2(a.x * a.y, a.y * a.z));
+}
+
+float naive(vec2 uv, float t) {
+  float mdist = 100.0;
+  float cidx  = 0.0;
+  float m     = 0.0;
+
+  for (float i = 0.0; i < 50.0; i++) {
+    vec2 n = N22(vec2(i));
+    vec2 p = sin(n * t);
+    float d = length(uv - p);
+
+    m += smoothstep(0.02, 0.01, d);
+
+    if (d < mdist) {
+      mdist = d;
+      cidx = i;
+    }
+  }
+
+  return mdist;
+  // return cidx;
+}
+
+void mainImage(out vec4 fragColor, in vec2 fragCoord) {
+  float t  = iTime;
+  vec3 res = iResolution;
+  vec2 fc  = fragCoord;
+
+  vec2 uv = (fc * 2.0 - res.xy) / res.y;
+
+  t *= 0.3;
+  // vec3 color = vec3(naive(uv, t) / 50.0) + vec3(N22(uv).xyx * 0.2);
+  vec3 color = vec3(naive(uv, t)) + vec3(N22(uv).xyx * 0.2);
+  fragColor = vec4(color, 1.0);
+}
+
--- goofin.glsl
+++ goofin.glsl
@@ -2,6 +2,36 @@
 precision mediump float;
 #endif
 
+float random (in vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))
+                 * 43758.5453123);
+}
+
+// 2D Noise based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise (in vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    // Four corners in 2D of a tile
+    float a = random(i);
+    float b = random(i + vec2(1.0, 0.0));
+    float c = random(i + vec2(0.0, 1.0));
+    float d = random(i + vec2(1.0, 1.0));
+
+    // Smooth Interpolation
+
+    // Cubic Hermine Curve.  Same as SmoothStep()
+    vec2 u = f*f*(3.0-2.0*f);
+    // u = smoothstep(0.,1.,f);
+
+    // Mix 4 coorners percentages
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
 float plot(vec2 st, float pct) {
   return smoothstep(pct - 0.01, pct, st.y) - smoothstep(pct, pct + 0.05, st.y);
 }
@@ -11,27 +41,40 @@ vec3 rgb(int r, int g, int b) {
   return normalize(p);
 }
 
+// 2D noise
+float noise2d(vec2 st) {
+    return fract(sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123);
+}
+
+// 1D noise
+float noise1d(float v){
+  return cos(v + cos(v * 90.1415) * 100.1415) * 0.5 + 0.5;
+}
+
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
-  // normalize the coordinate space between 0.0 and 1.0
-  vec2 st = (fragCoord * 4.0 - iResolution.xy * 2.0) / iResolution.y;
+  float t  = iTime;
+  vec3 res = iResolution;
+  vec2 fc  = fragCoord;
+
+  vec2 st = (fc * 4.0 - res.xy * 2.0) / res.y;
   // vec2 st = (fragCoord * 2.0 - iResolution.xy) / iResolution.y;
   // vec2 st = fragCoord / iResolution.xy;
 
-  ///-----------------------------//--
-  float y = sin(st.x + iTime);
-  // y = sin(st.x * 6.0 + iTime) + y;
+  vec3 color = vec3(0.0);
+  float f1 = sin(st.x + iTime);
+  float pct = plot(st, f1);
 
-  vec3 color = vec3(y);
-  float pct = plot(st, y);
+  color = pct * rgb(231, 160, 167) - rgb(169, 242, 255);
 
-  color = pct * vec3(0.0, 1.0, 0.0);
-
-  // sin(x * 10 + t) / 2 + F(x, t)
-  float px = sin(st.x * 10.0 + iTime)/2.0 + y;
-  pct = plot(st, px);
-  // color += pct * vec3(0.169, 0.749, 0.655);
-  // color += pct * normalizeRGB(vec3(231.0, 71.0, 167.0));
+  // float f2 = noise2d(st) * st.x + f1;
+  float f2 = noise(st) * st.x + f1;
+  pct = plot(st, f2);
   color += pct * rgb(231, 71, 167);
+
+  // float f3 = noise1d(st.x * 2.0 + t) + f1;
+  float f3 = noise(st) + f1;
+  pct = plot(st, f3);
+  color += pct * rgb(232, 97, 76);
 
   fragColor = vec4(color, 1.0);
 }
